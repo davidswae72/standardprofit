1a9183c5b86217708455eba91a2a65b9c854369a
davidswae72 <124398910+davidswae72@users.noreply.github.com>
2024-08-20 05:14:49 -0700

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <v-container style="overflow: hidden" fluid class="pa-0">
+
+  
     <v-row
       align="center"
       justify="center"
@@ -675,9 +677,7 @@ export default {
         {
           src: "https://cdn.jsdelivr.net/gh/coinponent/coinponent@undefined/dist/coinponent.js",
         },
-        {
-          src: "//code.tidio.co/v3p7xopjxhguxtzpbztl7gv5kzdtqpmc.js",
-        },
+     
       ],
       link: [
         {
